84b288249cefa98d2ae5f261b96aba3cbec884cd
mcnxo2001 <minhvietnam147@gmail.com>
2023-07-22 12:51:35 +0700

    Push to Github

---
+++ index.js
@@ -0,0 +1,14 @@
+let btCreateAccount = document.getElementById('btCreateAccount');
+let btLoginAccount = document.getElementById('btLoginAccount');
+let LoginArena = document.getElementById('LoginArena');
+let CreatAccountArea = document.getElementById('CreatAccountArea');
+
+btCreateAccount.addEventListener('click', function(){
+    CreatAccountArea.style.display = "block"
+    LoginArena.style.display = 'none';
+})
+
+btLoginAccount.addEventListener('click', function(){
+    CreatAccountArea.style.display = 'none';
+    LoginArena.style.display = 'block';
+})
