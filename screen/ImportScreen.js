15d06ee452688fa2411e174ee3d9e100a1915979
mcnxo2001 <minhvietnam147@gmail.com>
2023-07-27 22:01:45 +0700

    Update Add Data Function

--- screen/ImportScreen.js
+++ screen/ImportScreen.js
@@ -131,6 +131,14 @@ function addNewForm(f, e, a, b, c, d) {
     f.appendChild(NewformItem)
 }
 
+function submitData(a, b, c, d, index) {
+
+    arrWeight[index] = document.getElementById(a).value; //`weightFruit${idNumber}`
+    arrName[index] = document.getElementById(b).value; // `fruitName${idNumber}`
+    arrPerfectWeight[index] = document.getElementById(c).value; //`perfectweightFruit${idNumber}`
+    amountBox[index] = document.getElementById(d).value; //`amountBoxFruit${idNumber}`
+}
+
 btAddNewForm.addEventListener('click', function () {
     index++;
     addNewForm(formContent, `formItem${index}`, `fruitName${index}`, `amountBoxFruit${index}`, `weightFruit${index}`, `perfectweightFruit${index}`)
@@ -143,24 +151,22 @@ function fnDeteleForm(btID) {
 
 let btSubmitInformation = document.getElementById('btSubmitInformation');
 btSubmitInformation.addEventListener('click', function () {
-
-    for (let i = 0; i < formContent.children.length; i++) {
-        let idNumber = parseInt(formContent.children[i].id.substr(8))
-        arrWeight[i] = document.getElementById(`weightFruit${idNumber}`).value;
-        arrName[i] = document.getElementById(`fruitName${idNumber}`).value;
-        arrPerfectWeight[i] = document.getElementById(`perfectweightFruit${idNumber}`).value;
-        amountBox[i] = document.getElementById(`amountBoxFruit${idNumber}`).value;
-    }
     arrReport[1] = farmName.value;
     arrReport[2] = importCode.value;
     arrReport[0] = reportCode.value;
-
+    let idNumber;
+    for (let i = 0; i < formContent.children.length; i++) {
+        idNumber = parseInt(formContent.children[i].id.substr(8))
+        submitData(`weightFruit${idNumber}`, `fruitName${idNumber}`, `perfectweightFruit${idNumber}`, `amountBoxFruit${idNumber}`,i)
+    }
     console.log(arrWeight);
     console.log(arrPerfectWeight);
     console.log(amountBox);
     console.log(arrName);
     console.log(arrReport);
 })
+
+
 
 // retrive Arena
 
@@ -184,90 +190,4 @@ btRetriveInformation.addEventListener("click", function () {
         document.getElementById(`weightFruitEdit${i}`).value = arrWeight[i];
         document.getElementById(`perfectweightFruitEdit${i}`).value = arrPerfectWeight[i];
     }
-
-    // for (let i = 0; i < arrName.length; i++) {
-    //     let NewformItemEdit = document.createElement("form")
-    //     NewformItemEdit.setAttribute("id", `formItemEdit${i}`)
-    //     NewformItemEdit.setAttribute("class", "border p-3 rounded-3 border-primary mb-2")
-
-    //     let NewRowEdit = document.createElement("div")
-    //     NewRowEdit.setAttribute("class", "row")
-
-    //     let NewCol1 = document.createElement("div")
-    //     NewCol1.setAttribute("class", "col-12 col-sm-2 col-md-3 col-lg-3")
-    //     let ItemFormGroup1 = document.createElement("div") // create a div
-    //     ItemFormGroup1.setAttribute("class", "form-group mt-2") // set class for div
-    //     let ItemLabel1 = document.createElement("label") // creat a label
-    //     ItemLabel1.setAttribute("class", "fw-semibold") // set class for label
-    //     ItemLabel1.textContent = "Tên loại quả" // set text for label
-    //     let ItemInput1 = document.createElement("input") // create a input
-    //     ItemInput1.setAttribute("type", "text") // set type for input
-    //     ItemInput1.setAttribute("class", "form-control mt-2") // set class for input
-    //     ItemInput1.setAttribute("id", `fruitNameEdit${i}`) // set id for input
-    //     ItemInput1.value = arrName[i]
-
-    //     ItemFormGroup1.appendChild(ItemLabel1)
-    //     ItemFormGroup1.appendChild(ItemInput1)
-    //     NewCol1.appendChild(ItemFormGroup1)
-    //     NewRowEdit.appendChild(NewCol1)
-    //     NewformItemEdit.appendChild(NewRowEdit)
-
-    //     let NewCol4 = document.createElement("div")
-    //     NewCol4.setAttribute("class", "col-12 col-sm-2 col-md-3 col-lg-3")
-    //     let ItemFormGroup4 = document.createElement("div")
-    //     ItemFormGroup4.setAttribute("class", "form-group mt-2")
-    //     let ItemLabel4 = document.createElement("label")
-    //     ItemLabel4.setAttribute("class", "fw-semibold")
-    //     ItemLabel4.textContent = "Số lượng thùng"
-    //     let ItemInput4 = document.createElement("input")
-    //     ItemInput4.setAttribute("type", "text")
-    //     ItemInput4.setAttribute("class", "form-control mt-2")
-    //     ItemInput4.setAttribute("id", `amountBoxFruitEdit${i}`)
-    //     ItemInput4.value = amountBox[i]
-
-    //     ItemFormGroup4.appendChild(ItemLabel4)
-    //     ItemFormGroup4.appendChild(ItemInput4)
-    //     NewCol4.appendChild(ItemFormGroup4)
-    //     NewRowEdit.appendChild(NewCol4)
-    //     NewformItemEdit.appendChild(NewRowEdit)
-
-    //     let NewCol2 = document.createElement("div")
-    //     NewCol2.setAttribute("class", "col-12 col-sm-2 col-md-3 col-lg-3")
-    //     let ItemFormGroup2 = document.createElement("div")
-    //     ItemFormGroup2.setAttribute("class", "form-group mt-2")
-    //     let ItemLabel2 = document.createElement("label")
-    //     ItemLabel2.setAttribute("class", "fw-semibold")
-    //     ItemLabel2.textContent = "Trọng lượng mỗi thùng"
-    //     let ItemInput2 = document.createElement("input")
-    //     ItemInput2.setAttribute("type", "text")
-    //     ItemInput2.setAttribute("class", "form-control mt-2")
-    //     ItemInput2.setAttribute("id", `weightFruitEdit${i}`)
-    //     ItemInput2.value = arrWeight[i]
-
-    //     ItemFormGroup2.appendChild(ItemLabel2)
-    //     ItemFormGroup2.appendChild(ItemInput2)
-    //     NewCol2.appendChild(ItemFormGroup2)
-    //     NewRowEdit.appendChild(NewCol2)
-    //     NewformItemEdit.appendChild(NewRowEdit)
-
-    //     let NewCol3 = document.createElement("div")
-    //     NewCol3.setAttribute("class", "col-12 col-sm-2 col-md-3 col-lg-3")
-    //     let ItemFormGroup3 = document.createElement("div")
-    //     ItemFormGroup3.setAttribute("class", "form-group mt-2")
-    //     let ItemLabel3 = document.createElement("label")
-    //     ItemLabel3.setAttribute("class", "fw-semibold")
-    //     ItemLabel3.textContent = "Trọng lượng hàng tốt"
-    //     let ItemInput3 = document.createElement("input")
-    //     ItemInput3.setAttribute("type", "text")
-    //     ItemInput3.setAttribute("class", "form-control mt-2")
-    //     ItemInput3.setAttribute("id", `perfectweightFruitEdit${i}`)
-    //     ItemInput3.value = arrPerfectWeight[i]
-
-    //     ItemFormGroup3.appendChild(ItemLabel3)
-    //     ItemFormGroup3.appendChild(ItemInput3)
-    //     NewCol3.appendChild(ItemFormGroup3)
-    //     NewRowEdit.appendChild(NewCol3)
-    //     NewformItemEdit.appendChild(NewRowEdit)
-    //     formContentEdit.appendChild(NewformItemEdit)
-    // }
 })
